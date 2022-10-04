d30d730af819639495e31dfa2ff503e2de7bc920
usamabhatti <bhattiusama003@gmail.com>
2022-10-04 14:31:01 +0500

    comment

--- EcommProject/Controllers/ProductController.cs
+++ EcommProject/Controllers/ProductController.cs
@@ -18,7 +18,8 @@ namespace EcommProject.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        ///asdsadsadsasdfffa
+        public async Task<IActionResult> GetALL()
         {
             var record = await productRepository.GetAllRecordAsync();
             return Ok(record);
